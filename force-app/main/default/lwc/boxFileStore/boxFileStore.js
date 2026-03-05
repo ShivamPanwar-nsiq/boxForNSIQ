e57282c09584c8ac96f6243751ae290cfdd7c31d
shivampanwar697 <shivampanwar697@gmail.com>
2026-03-05 14:38:19 +0530

    show file on the main cmp boxdocumentManager

---
+++ force-app/main/default/lwc/boxFileStore/boxFileStore.js
@@ -0,0 +1,37 @@
+// Global store for Box files
+
+const files = [];
+
+const listeners = new Set();
+
+export function addFiles(newFiles){
+
+    newFiles.forEach(file => {
+        files.push(file);
+    });
+
+    notifyListeners();
+
+}
+
+export function getFiles(){
+
+    return files;
+
+}
+
+export function registerListener(listener){
+
+    listeners.add(listener);
+
+}
+
+function notifyListeners(){
+
+    listeners.forEach(listener => {
+
+        listener([...files]);
+
+    });
+
+}
